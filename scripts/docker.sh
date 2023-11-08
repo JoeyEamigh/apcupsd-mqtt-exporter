536311c51ea55475745558db23d000586d875f01
Joey Eamigh <55670930+JoeyEamigh@users.noreply.github.com>
2023-11-08 15:08:32 -0500

    updating docker image src

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -5,8 +5,7 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-docker buildx build . -t ghcr.io/joeyeamigh/apcupsd-mqtt-exporter/apcupsd-mqtt-exporter:$1
-docker tag ghcr.io/joeyeamigh/apcupsd-mqtt-exporter/apcupsd-mqtt-exporter:$1 ghcr.io/joeyeamigh/apcupsd-mqtt-exporter/apcupsd-mqtt-exporter:latest
+docker buildx build . -t ghcr.io/joeyeamigh/apcupsd-mqtt-exporter:$1 -t ghcr.io/joeyeamigh/apcupsd-mqtt-exporter:latest
 
-docker push ghcr.io/joeyeamigh/apcupsd-mqtt-exporter/apcupsd-mqtt-exporter:$1
-docker push ghcr.io/joeyeamigh/apcupsd-mqtt-exporter/apcupsd-mqtt-exporter:latest
+docker push ghcr.io/joeyeamigh/apcupsd-mqtt-exporter:$1
+docker push ghcr.io/joeyeamigh/apcupsd-mqtt-exporter:latest
